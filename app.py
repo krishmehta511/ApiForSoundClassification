90225c17e551799fdcec41c442100759e873de41
Krish Mehta <98233063+krishmehta511@users.noreply.github.com>
2022-10-31 20:41:09 +0530

    Adding /pred api

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import tensorflow as tf
+import librosa
+import numpy as np
 import pickle
 
 app = Flask(__name__)
@@ -8,9 +10,23 @@ app = Flask(__name__)
 labelencoder = pickle.load(open('static/label_encoder.pkl', 'rb'))
 model = tf.keras.models.load_model('static/SavedModel')
 
+
+def getFeatures(audioInput):
+    audioData, audioSampleRate = librosa.load(audioInput, res_type="kaiser_fast")
+    features = librosa.feature.mfcc(y=audioData, sr=audioSampleRate, n_mfcc=40)
+    scaled_features = np.mean(features.T, axis=0)
+    return scaled_features
+
+
 @app.route('/')
 def index():
     return 'hello'
+
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    return 'pred'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
