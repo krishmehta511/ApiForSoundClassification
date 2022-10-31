73081c5c407a472d8eac2f8878f9262f99af29e1
Krish Mehta <98233063+krishmehta511@users.noreply.github.com>
2022-10-31 20:30:21 +0530

    Adding model and label encoder

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from cProfile import run
 from flask import Flask
+import tensorflow as tf
+import pickle
 
 app = Flask(__name__)
 
+
+labelencoder = pickle.load(open('static/label_encoder.pkl', 'rb'))
+model = tf.keras.models.load_model('static/SavedModel')
+
 @app.route('/')
 def index():
-    return 'Hello'
+    return 'hello'
 
 if __name__ == "__main__":
     app.run(debug=True)
